d6c0b42e480c2fccaa7889603eebf90bdb88e6c0
aster03 <hjh20030615@gmail.com>
2024-06-01 01:42:34 +0900

    Lucy_week5

---
+++ app/src/main/java/com/example/a6th_umc_flo/LockerFragment.kt
@@ -0,0 +1,23 @@
+package com.example.a6th_umc_flo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.a6th_umc_flo.databinding.FragmentLockerBinding
+
+class LockerFragment : Fragment() {
+
+    lateinit var binding: FragmentLockerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLockerBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
